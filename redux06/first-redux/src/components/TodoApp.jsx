69d8489256ea3d2958076a23c5f3f53608225ef0
Leekwanyong <dlrkdid12@gmail.com>
2025-02-04 14:37:33 +0900

    feat: redux hook 실습

--- redux06/first-redux/src/components/TodoApp.jsx
+++ redux06/first-redux/src/components/TodoApp.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    addTodo as addTodoActionCreator,
+    removeTodo as removeAllActionCreator,
+    removeAll as removeTodoActionCreator,
+} from "../slices/todoSlice";
+import { fetchTodosRequested as fetchTodosRequestedActionCreator } from "../actions/fetchTodosAction";
 
 export default function TodoApp(props) {
-    const { todoItems, addTodo, removeTodo, removeAll, triggerAsyncFunction, fetchTodo } = props;
+    // const { todoItems, addTodo, removeTodo, removeAll, triggerAsyncFunction, fetchTodo } = props;
+    const todoItems = useSelector((state) => [...state.todo, ...state.fetchTodo.data]);
+    const dispatch = useDispatch();
     const [newTodo, setNewTodo] = useState("");
     return (
         <div>
@@ -14,13 +23,31 @@ export default function TodoApp(props) {
 
             <div>
                 <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-                <button onClick={() => addTodo(newTodo)}>할 일 추가</button>
-                <button onClick={removeTodo}>할 일 삭제</button>
-                <button onClick={removeAll}>모두 삭제</button>
+                <button
+                    onClick={() => {
+                        dispatch(addTodoActionCreator(newTodo));
+                    }}
+                >
+                    할 일 추가
+                </button>
+                <button
+                    onClick={() => {
+                        dispatch(removeTodoActionCreator());
+                    }}
+                >
+                    할 일 삭제
+                </button>
+                <button
+                    onClick={() => {
+                        dispatch(removeAllActionCreator());
+                    }}
+                >
+                    모두 삭제
+                </button>
             </div>
             <button
                 onClick={() => {
-                    triggerAsyncFunction((dispatch, getState) => {
+                    dispatch((dispatch, getState) => {
                         console.log("비동기 함수 실행", getState());
 
                         new Promise((resolve, reject) => {
@@ -37,7 +64,13 @@ export default function TodoApp(props) {
             >
                 비동기 함수 테스트
             </button>
-            <button onClick={fetchTodo}>서버에서 할 일 목록 받아오기</button>
+            <button
+                onClick={() => {
+                    dispatch(fetchTodosRequestedActionCreator());
+                }}
+            >
+                서버에서 할 일 목록 받아오기
+            </button>
         </div>
     );
 }
--- redux06/first-redux/src/index.jsx
+++ redux06/first-redux/src/index.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import store, { persistor } from "./rudux/store.js";
-import TodoAppContainer from "./rudux/containers/TodoAppContainer.js";
 import { PersistGate } from "redux-persist/integration/react";
+import TodoApp from "./components/TodoApp.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <TodoAppContainer />
+                <TodoApp />
             </PersistGate>
         </Provider>
     </React.StrictMode>
